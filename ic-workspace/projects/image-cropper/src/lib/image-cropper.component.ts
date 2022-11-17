d741df76683e3d34718e45cdb983a85479dc1352
Alexander Eberle <alexander171294@gmail.com>
2022-11-17 17:01:13 -0300

    initial library

---
+++ ic-workspace/projects/image-cropper/src/lib/image-cropper.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-image-cropper',
+  template: `
+    <p>
+      image-cropper works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ImageCropperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ic-workspace/projects/image-cropper/src/lib/image-cropper.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ImageCropperComponent } from './image-cropper.component';
+
+
+
+@NgModule({
+  declarations: [
+    ImageCropperComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ImageCropperComponent
+  ]
+})
+export class ImageCropperModule { }
